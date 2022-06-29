a5c1a002fa0d0c76e0cdcb6a26a976d5d26e5350
Gui Barreto <barretogui90@gmail.com>
2022-06-29 12:04:50 -0300

    fix table in Calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,71 @@
 function getCalculator() {
-        return {
-            display: document.querySelector('.display'),
-
-
+    return {
+        display: document.querySelector('.display'),
+        
             start: function() {
-                alert('Initiate Calculator')
+                this.clickButtons()
+                this.pressEnter()
             },
-            
-            clickButtons() {
-                document.addEventListener('click', function(e) {
-                    const el = e.target
 
-                    if(el.classList.contains('btn-num')) {
-                        this.btnToDisplay()
+            pressEnter() {
+                this.display.addEventListener('keyup', e => {
+                    if(e.keyCode === 13) {
+                        this.calculation()
                     }
                 })
+            },
+
+            clearDisplay() {
+                this.display.value = ''
+            },
+
+            deleteLast() {
+                this.display.value = this.display.value.slice(0, -1)
+            },
+
+            calculation() {
+                let mathAccount = this.display.value
+
+                try{
+                    mathAccount = eval(mathAccount)
+
+                    if(!mathAccount) {
+                        alert('Invalid account')
+                        return
+                    }
+                    
+                    this.display.value = String(mathAccount)
+                } catch(e) {
+                    alert('Invalid account')
+                    return
+                }
+            },
+
+            
+            clickButtons() {
+                document.addEventListener('click', e => {
+                    const el = e.target
+                    
+                    if(el.classList.contains('btn-num')) {
+                        this.btnToDisplay(el.innerText)
+                    }
+
+                    if(el.classList.contains('btn-clear')) {
+                        this.clearDisplay()
+                    }
+
+                    if(el.classList.contains('btn-del')) {
+                        this.deleteLast()
+                    }
+
+                    if(el.classList.contains('btn-eq')) {
+                        this.calculation()
+                    }
+                })
+            },
+            
+            btnToDisplay(amount) {
+                this.display.value += amount
             }
         }
 }
